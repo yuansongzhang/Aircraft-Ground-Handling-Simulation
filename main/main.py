f5d2d2cdc43d8823681a3600c24afcbab2d3695e
Mxy1210 <1953504@tongji.edu.cn>
2021-12-15 23:32:38 +0800

    main

---
+++ main/main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from Aircraft import Aircraft
+from Algorithm import Algorithm
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('hi')
+    
+def main():
+    aircraft = Aircraft()
+    vehicle = Vehicle()
+    algorithm = Algorithm()
+    Object = [aircraft,vehicle,algorithm]
+    triggeringTimestamp = 
+    
+    for obj in Object:
+        if obj.event.triggeringTimestamp < triggeringTimestamp:
+            triggeringTimestamp = obj.event.triggeringTimestamp
+            event = obj.event
+            
+        event.update()
+        event.execute()
+    
+    
+    
+
