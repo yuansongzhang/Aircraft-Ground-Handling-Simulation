85674c47fe354d31c9cfb70c60c3afc58ef75587
akinal0103 <632065123@qq.com>
2022-01-18 16:35:05 +0800

    utils/reader.py

--- utils/reader.py
+++ utils/reader.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-from description import Flight
-
+import pandas as pd
+#from description import flight
 
 def generate_flights(filename):
-    # todo
-    pass
+    file = pd.read_excel(filename, encoding = 'utf-8')
+    df = pd.DataFrame(file)
+    sample = df.sample(n = 1)
+    del sample['机型']
+    sample.columns = ['航班日期', '航班号', '到港/离港', '机位', '计划到达时间', '实际到达时间', 
+                      '计划起飞时间', '实际起飞时间']  
+    return sample
